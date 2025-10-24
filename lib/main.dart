cf7e7cafc3f3f07c27c93fe95727d566c266e4b9
MariosGiagkou <mariosyiangou99@gmail.com>
2025-10-24 15:49:34 +0100

    create new class

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
   const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'My sandwich shop',
       theme: ThemeData(
         // This is the theme of your application.
